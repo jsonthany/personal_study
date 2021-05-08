e090ffed6e889c06efe009e79ff95d815f4ec0d2
jsonthany <jason.thany@outlook.com>
2021-05-07 23:40:53 -0700

    completed array module

--- data_structures_&_algorithm/Arrays/ArrayAlgo.py
+++ data_structures_&_algorithm/Arrays/ArrayAlgo.py
@@ -82,3 +82,96 @@ print(plusOneC([9,9,9]) == [1,0,0,0])
 print(plusOneC([0]) == [1])
 print(plusOneC([1,2,5]) == [1,2,6])
 print(plusOneC([9]) == [1,0])
+
+
+# TWO SUM PROBLEM
+
+# Given an array of integers, return True or False if the array has two numbers that add up to a specific target. 
+# Assumption: You may assume that each input would have exactly one solution.
+# Link: https://www.educative.io/courses/ds-and-algorithms-in-python/JYpB2qkNrDo
+
+def sumTwoPartsA(arrayList, target):
+    
+    for i in range(len(arrayList) - 1):
+        for j in range(1, len(arrayList)):
+            if ((arrayList[i] + arrayList[j]) == target):
+                print(arrayList[i], arrayList[j])
+                return True
+    return False
+
+print("")
+sumTwoPartsA([-2, 1, 2, 4, 7, 11], 13) # should be True
+print(sumTwoPartsA([-2, 1, 2, 4, 7, 11], 13))
+sumTwoPartsA([-2, 1, 2, 4, 7, 11], 20) # should be False
+print(sumTwoPartsA([-2, 1, 2, 4, 7, 11], 20))
+
+def sumTwoPartsB(arrayList, target):
+    
+    dictValues = dict()
+    
+    for num in arrayList:
+        if (num in dictValues):
+            print(dictValues[num], num)
+            return True
+        else:
+            dictValues[(target - num)] = num
+            
+    return False
+
+print("")
+sumTwoPartsB([-2, 1, 2, 4, 7, 11], 13) # should be True
+print(sumTwoPartsB([-2, 1, 2, 4, 7, 11], 13))
+sumTwoPartsB([-2, 1, 2, 4, 7, 11], 20) # should be False
+print(sumTwoPartsB([-2, 1, 2, 4, 7, 11], 20))
+
+def sumTwoPartsC(arrayList, target):
+    
+    a = 0
+    b = len(arrayList) - 1
+    arrayList.sort()
+    
+    while (a < b):
+        if ((arrayList[a] + arrayList[b]) == target):
+            print(arrayList[a], arrayList[b])
+            return True
+        if ((arrayList[a] + arrayList[b]) < target):
+            a += 1
+        if ((arrayList[a] + arrayList[b]) > target):
+            b -= 1
+    
+    return False
+
+print("")
+sumTwoPartsC([-2, 1, 2, 4, 7, 11], 13) # should be True
+print(sumTwoPartsC([-2, 1, 2, 4, 7, 11], 13))
+sumTwoPartsC([-2, 1, 2, 4, 7, 11], 20) # should be False
+print(sumTwoPartsC([-2, 1, 2, 4, 7, 11], 20))
+
+# OPTIMAL TASK ASSIGNMENT
+
+# Assign tasks to workers so that the time it takes to complete all the tasks is minimized given a count of 
+# ... workers and an array where each element indicates the duration of a task.
+
+# bitwise complement operator:
+#  i | ~i  
+# --------
+#  0 | -1
+#  1 | -2
+#  2 | -3
+#  3 | -4 
+#  4 | -5 
+#  5 | -6
+
+# this is using the GREEDY APPROACH
+def optimalTaskAssignment(arrayList):
+    
+    arrayList.sort()
+    
+    for i in range(len(arrayList) // 2):
+        print(arrayList[i], arrayList[~i])
+        
+    if (len(arrayList) % 2 == 1):
+        print(arrayList[(len(arrayList) // 2) + 1], 0)
+        
+print("")
+optimalTaskAssignment([6, 3, 2, 7, 5, 5, 5])
